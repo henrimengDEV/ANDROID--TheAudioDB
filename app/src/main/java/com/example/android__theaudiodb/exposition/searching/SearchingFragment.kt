735269f6fca074d136149017fb4331e9c83121c6
Mickael <akninemickael@gmail.com>
2022-03-01 22:41:50 +0100

    optimize import

--- app/src/main/java/com/example/android__theaudiodb/exposition/searching/SearchingFragment.kt
+++ app/src/main/java/com/example/android__theaudiodb/exposition/searching/SearchingFragment.kt
@@ -3,14 +3,10 @@ package com.example.android__theaudiodb.exposition.searching
 import android.app.SearchManager
 import android.content.Context
 import android.os.Bundle
-import android.text.method.TextKeyListener.clear
-import android.util.Log
-import android.view.MenuItem
 import android.view.View
 import android.widget.ProgressBar
-import androidx.appcompat.widget.SearchView
 import android.widget.Toast
-import androidx.core.view.get
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Lifecycle
