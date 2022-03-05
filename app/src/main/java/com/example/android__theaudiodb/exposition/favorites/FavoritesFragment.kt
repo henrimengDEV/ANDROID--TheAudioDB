22e4f4e80450c6870eae2fbd3763e716bee9910e
Mickael <akninemickael@gmail.com>
2022-03-05 22:03:55 +0100

    essai de la BDD sqlite

--- app/src/main/java/com/example/android__theaudiodb/exposition/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/example/android__theaudiodb/exposition/favorites/FavoritesFragment.kt
@@ -6,15 +6,23 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android__theaudiodb.R
+import com.example.android__theaudiodb.domain.artist.Artist
 import com.example.android__theaudiodb.exposition.shared.adapter.AlbumsRecyclerViewAdapter
 import com.example.android__theaudiodb.exposition.shared.adapter.ArtistsRecyclerViewAdapter
 import com.example.android__theaudiodb.infrastructure.InMemoryAlbumsRepository
 import com.example.android__theaudiodb.infrastructure.InMemoryArtistsRepository
+import com.example.android__theaudiodb.infrastructure.SQLite.DatabaseManager
+import com.example.android__theaudiodb.infrastructure.SQLite.entity.ArtistEntity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
 
 class FavoritesFragment : Fragment(R.layout.fragment_favorites) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         setUpArtistsRecyclerView(view)
         setUpAlbumsRecyclerView(view)
+
     }
 
     private fun setUpArtistsRecyclerView(view: View) {
---
+++ app/src/main/java/com/example/android__theaudiodb/infrastructure/SQLite/DatabaseManager.kt
@@ -0,0 +1,27 @@
+package com.example.android__theaudiodb.infrastructure.SQLite
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.android__theaudiodb.infrastructure.SQLite.entity.ArtistEntity
+import com.example.android__theaudiodb.infrastructure.dao.ArtistDAO
+
+@Database(entities = [ArtistEntity::class], version = 1)
+abstract class DatabaseManager : RoomDatabase() {
+    abstract fun dao(): ArtistDAO?
+
+    companion object {
+        // Singleton
+        private var sInstance: DatabaseManager? = null
+        fun getDatabase(context: Context): DatabaseManager? {
+            if (sInstance == null) {
+                sInstance = Room.databaseBuilder(
+                    context.applicationContext,
+                    DatabaseManager::class.java, "db.sqlite"
+                ).build()
+            }
+            return sInstance
+        }
+    }
+}
